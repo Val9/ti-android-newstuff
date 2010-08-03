58d877aef3ca216bc875aca48c29fb154aaf2754
Bill Dawson <bill@billdawson.com>
2010-08-03 09:34:29 +0200

    Initial commit

---
+++ res/Resources/app.js
@@ -0,0 +1,8 @@
+Titanium.UI.setBackgroundColor('#000');
+
+Titanium.UI.createWindow({  
+	url: 'main.js',
+    title:'New Titanium Stuff',
+	exitOnClose: true,
+    backgroundColor:'#fff'
+}).open();
